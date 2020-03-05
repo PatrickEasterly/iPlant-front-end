6bbbdf1b8a41c6ea0f338e6236da848096f76a24
Catie Evans <catieevans@Caties-MacBook-Pro.local>
2020-03-05 15:18:00 -0500

    working on adding cards instead of list items

--- src/Pages/Add.js
+++ src/Pages/Add.js
@@ -15,7 +15,11 @@ import {
   Icon,
   ListItem,
   Header, 
-  Thumbnail
+  Thumbnail,
+  Left,
+  Right,
+  Body,
+  List
 } from 'native-base';
 import axios from 'axios';
 
@@ -65,7 +69,39 @@ export default class AddPlant extends React.Component {
     return employees.filter(employee => employee.latinname.search(regex) >= 0);
   }
 
+  renderPlant(employees) {
+    const emp = employees.length === 1 && comp(query, employees[0].latinname)
+    ? [] : employees
+    return this.state.employees.map((emp) => {
+      return (
+      <ListItem thumbnail
+      onPress={() => {
+        (this.setState({ query: emp.commonname, selectedEmployee: emp }))
+          }
+        }
+        >
+        <Left>
+              <Thumbnail square source={{ uri: emp.photo }} />
+            </Left>
+            <Body>
+              <Text>{emp.latinname}</Text>
+              <Text note numberOfLines={1}>{emp.commonname}</Text>
+            </Body>
+            <Right>
+              <Button transparent>
+                <Text>View</Text>
+              </Button>
+            </Right>
+        {/* <Text>
+          <Thumbnail small source={{uri: emp.photo}} />
+          {emp.latinname}({emp.commonname})
+        </Text> */}
+      </ListItem>
+    )
+})
 
+}
+  
   render() {
     const { query, selectedEmployee } = this.state;
     const employees = this.findEmployee(query);
@@ -74,12 +110,16 @@ export default class AddPlant extends React.Component {
     return (
       <Container>
       <Header searchBar rounded>
-        <Item>
+        <Item regular>
           <Icon name="ios-search" />
-          {/* <Input placeholder="Search" /> */}
-          <View style={styles.autoComplete}> 
-
-            <Autocomplete
+          <Input placeholder="Search" onChangeText={text => this.setState({ query: text })} placeholder="Enter plant name" defaultValue={query}/>
+          {/* <View style={styles.autoComplete}>  */}
+        </Item>
+        <Button transparent>
+          <Text>Search</Text>
+        </Button>
+      </Header>
+            {/* <Autocomplete
               autoCapitalize="none"
               autoCorrect={false}
               data={employees.length === 1 && comp(query, employees[0].latinname)
@@ -89,25 +129,70 @@ export default class AddPlant extends React.Component {
               onChangeText={text => this.setState({ query: text })}
               placeholder="Enter plant name"
               renderItem={emp =>
-                <ListItem
-                  onPress={() => {
-                    (this.setState({ query: emp.commonname, selectedEmployee: emp }))
-                  }
-                }
+               */}
+              {/* //   <Container>
+              //   <Header />
+              //   <Content>
+              //     <List>
+              //       <ListItem thumbnail>
+                      // <Left>
+                      //   <Thumbnail square source={{ uri: 'Image URL' }} />
+                      // </Left>
+                      // <Body>
+                      //   <Text>Sankhadeep</Text>
+                      //   <Text note numberOfLines={1}>Its time to build a difference . .</Text>
+                      // </Body>
+                      // <Right>
+                      //   <Button transparent>
+                      //     <Text>View</Text>
+                      //   </Button>
+                      // </Right>
+              //       </ListItem>
+              //     </List>
+              //   </Content>
+              // </Container>
+              // <Content> */}
+                <List
+                 
+                // {this.state.employees.map((employee) => {
+                //   <ListItem thumbnail
+                //   onPress={() => {
+                //     (this.setState({ query: emp.commonname, selectedEmployee: emp }))
+                //   }
+                // }
+                // >
+                //     <Left>
+                //           <Thumbnail square source={{ uri: emp.photo }} />
+                //         </Left>
+                //         <Body>
+                //           <Text>{emp.latinname}</Text>
+                //           <Text note numberOfLines={1}>{emp.commonname}</Text>
+                //         </Body>
+                //         <Right>
+                //           <Button transparent>
+                //             <Text>View</Text>
+                //           </Button>
+                //         </Right>
+                //     {/* <Text>
+                //       <Thumbnail small source={{uri: emp.photo}} />
+                //       {emp.latinname}({emp.commonname})
+                //     </Text> */}
+                //   </ListItem>
+                // })}
                 >
-                  <Text>
-                    <Thumbnail small source={{uri: emp.photo}} />
-                    {emp.latinname}({emp.commonname})
-                  </Text>
-                </ListItem>}
-            />
-          </View>
+                  <View>
+                    {() => this.renderPlant(employees)}
+
+                  </View>
+                
+               </List>
+            {/* </Content> */}
+              {/* }
+            /> */}
+          {/* </View> */}
           {/* <Icon name="ios-people" /> */}
-        </Item>
-        <Button transparent>
-          <Text>Search</Text>
-        </Button>
-      </Header>
+       
+       
     </Container>
     );
   }
--- src/Pages/MyPlants.js
+++ src/Pages/MyPlants.js
@@ -81,6 +81,10 @@ export default function MyPlants() {
   )
 }
 
+
+  
+
+
 const styles = {
   hiddenCard: {
     display: 'none'
