142c8588b535d935a56e64a9451cac7e868bd6cd
Catie Evans <catieevans@Caties-MacBook-Pro.local>
2020-03-06 00:51:47 -0500

    adds styling to search feature

--- src/Pages/Add.js
+++ src/Pages/Add.js
@@ -4,7 +4,8 @@ import {
   StyleSheet,
   View
 } from 'react-native';
-
+// import { StackNavigator } from "react-navigation";
+import axios from 'axios';
 import {
   Container,
   Content,
@@ -19,23 +20,21 @@ import {
   Body,
   Left,
   Right,
-  ListView
+  ActionSheet
 } from 'native-base';
-import axios from 'axios';
 
 // const API = 'http://localhost:5000/api/plantinfo';
 
 const API = 'http://192.168.1.132:5000/api/plantinfo';
-// const {vw, vh, vmin, vmax} = require('react-native-viewport-units');
-
-
-
+// var BUTTONS = ["Option 0", "Option 1", "Option 2", "Delete", "Cancel"];
+// var DESTRUCTIVE_INDEX = 3;
+// var CANCEL_INDEX = 4;
 
 
 export default class AddPlant extends React.Component {
   static renderEmployee(employee) {
     const { latinname, commonname } = employee;
-
+    
     return (
       <View>
         <Text style={styles.directorText}>$({commonname})</Text>
@@ -43,36 +42,38 @@ export default class AddPlant extends React.Component {
       </View>
     );
   }
-
+  
   constructor(props) {
     super(props);
     this.state = {
       employees: [],
       query: '',
       selectedEmployee: null,
+      // clicked: ''
     };
   }
-
+  
   componentDidMount() {
     axios.get(API)
-        .then((res) => {
-          console.log(res)
-          this.setState({ employees: res.data });
-      });
+    .then((res) => {
+      console.log(res)
+      this.setState({ employees: res.data });
+    });
   }
-
+  
   findEmployee(query) {
     if (query === '') {
       return [];
     }
-
+    
     const { employees } = this.state;
     const regex = new RegExp(`${query.trim()}`, 'i');
     return employees.filter(employee => employee.latinname.search(regex) >= 0);
   }
-
-
+  
+  
   render() {
+ 
     const { query, selectedEmployee } = this.state;
     const employees = this.findEmployee(query);
     const comp = (a, b) => a.toLowerCase().trim() === b.toLowerCase().trim();
@@ -82,7 +83,7 @@ export default class AddPlant extends React.Component {
         <Item style={styles.scroll}>
           <Icon name="ios-search" />
           {/* <Input placeholder="Search" /> */}
-          <View style={styles.autoComplete}> 
+        <View style={styles.autoComplete}> 
         
             <Autocomplete
               autoCapitalize="none"
@@ -93,42 +94,77 @@ export default class AddPlant extends React.Component {
               hideResults={selectedEmployee && selectedEmployee.latinname === query}
               onChangeText={text => this.setState({ query: text })}
               placeholder="Enter plant name"
-              listStyle= {{flex: 1, maxHeight:800}}
+              inputContainerStyle={{}}
+              containerStyle={{minWidth: '100%'}}
+              listContainerStyle={{minWidth: '100%'}}
+              listStyle= {{flex: 2, maxHeight:800, minWidth: 375}}
               renderItem={emp =>
-                // <ListView>
-                
-                <ListItem thumbnail style={styles.listStyle}
+                <View style={{marginRight: 20}}>
+               
+
+                <ListItem thumbnail style={{margin:6, marginLeft: 0, paddingLeft: 0,}}
                 onPress={() => {
                   (this.setState({ query: emp.commonname, selectedEmployee: emp }))
                 }
               }
               >
-                  <Left>
-                    <Thumbnail square source={{ uri: emp.photo }} />
+                  <Left style={{marginLeft: 0, paddingLeft: 0}}>
+                    <Thumbnail style={{marginLeft: 0, paddingLeft: 0, borderRadius: 5, height: 70, width: 70,
+              
+                      shadowColor: '#0000',
+                      shadowOffset: { width: 0, height: 1 },
+                      shadowOpacity: 0.8,
+                      shadowRadius: 50,  
+                  
+                    }} square source={{ uri: emp.photo }} />
                   </Left>
                   <Body>
-                    <Text>{emp.latinname}</Text>
-                    <Text note numberOfLines={1}>{emp.commonname}</Text>
+                    <Text style={{padding: 2, textTransform:'capitalize'}}>{emp.commonname}</Text>
+                    <Text style={{padding: 3, textTransform:'capitalize'}} note numberOfLines={1}>{emp.latinname}</Text>
                   </Body>
                   <Right>
                     <Button transparent>
-                      <Text>View</Text>
+                   
+                      <Icon name="arrow-forward" />
+              
                     </Button>
                   </Right>
                   </ListItem>
-                // </ListView>
+              </View>
               }
               />
               </View>
           
+          
+          
 
            
-          {/* <Icon name="ios-people" /> */}
+        
         </Item>
         <Button transparent>
           <Text>Search</Text>
         </Button>
       </Header>
+      {/* <Content>
+              <Button
+                  onPress={() => {
+                    ActionSheet.show(
+                      {
+                        options: BUTTONS,
+                        cancelButtonIndex: CANCEL_INDEX,
+                        destructiveButtonIndex: DESTRUCTIVE_INDEX,
+                        title: "Testing ActionSheet"
+                      },
+                      buttonIndex => {
+                        this.setState({ clicked: BUTTONS[buttonIndex] });
+                      }
+                    )}
+                  }
+                >
+            <Text>Actionsheet</Text>
+          </Button> 
+
+      </Content>  */}
     </Container>
     );
   }
@@ -137,24 +173,18 @@ export default class AddPlant extends React.Component {
 
 const styles = {
   autoComplete: {
-    // textDecoration:'none',
-    flex: 1,
-    // border:'none',
-    // overflow: 'hidden',
-    // position: 'relative'
+   
+    flex: 2,
+ 
   },
   scroll:{
-    // overflow: 'hidden',
-    // position: 'relative'
+    overflow: 'auto',
+    
   },
   textinput: {
     border:'none',
   },
-  listStyle: {
-    flex: 1,
-    // width: '100%',
-    padding: 0
-  }
+ 
 }
 
 
